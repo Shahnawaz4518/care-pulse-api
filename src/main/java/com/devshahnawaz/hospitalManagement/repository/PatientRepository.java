12568881ab46b4d0b3a1faa5951cf56a857151d3
Shahnawaz Khan <shahnawaz.khan@cwdin.com>
2026-05-11 12:47:16 +0530

    added maually auery for getting specific data from database

--- src/main/java/com/devshahnawaz/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/devshahnawaz/hospitalManagement/repository/PatientRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.devshahnawaz.hospitalManagement.entity.Patient;
+import com.devshahnawaz.hospitalManagement.entity.type.BloodGroupType;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
@@ -18,4 +21,7 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
     List<Patient> findByBirthDateBetween(LocalDate starDate, LocalDate endDate);
 
     List<Patient> findByNameContainingOrderByIdDesc(String query);
+
+    @Query("SELECT p FROM Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
 }
--- src/test/java/com/devshahnawaz/hospitalManagement/PatientTest.java
+++ src/test/java/com/devshahnawaz/hospitalManagement/PatientTest.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devshahnawaz.hospitalManagement.entity.Patient;
+import com.devshahnawaz.hospitalManagement.entity.type.BloodGroupType;
 import com.devshahnawaz.hospitalManagement.repository.PatientRepository;
 import com.devshahnawaz.hospitalManagement.service.PatientService;
 
@@ -21,9 +24,10 @@ public class PatientTest {
 
     @Autowired
     private PatientService patientService;
+
     @Test
     public void testPatientRepository() {
-        
+
         List<Patient> patientList = patientRepository.findAll();
         System.out.println(patientList);
 
@@ -32,16 +36,25 @@ public class PatientTest {
     }
 
     @Test
-    public void testTransactionMethods(){
+    public void testTransactionMethods() {
         // Patient patient = patientService.getPatientById(1L);
 
         // Patient patient = patientRepository.findByName("Priya Verma");
 
-        // List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1993,02, 10),"kavita.nair@gmail.com");
-        List<Patient> patientList = patientRepository.findByNameContainingOrderByIdDesc("h");
-        
-        for(Patient patient: patientList){
-        System.out.println(patient);
+        // List<Patient> patientList =
+        // patientRepository.findByBirthDateOrEmail(LocalDate.of(1993,02,
+        // 10),"kavita.nair@gmail.com");
+        // List<Patient> patientList =
+        // patientRepository.findByNameContainingOrderByIdDesc("h");
+
+        // for(Patient patient: patientList){
+        // System.out.println(patient);
+        // }
+
+        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+
+        for (Patient patient : patientList) {
+            System.out.println(patient);
+        }
     }
-  }
 }
